3fc1e65484989da6c8dce754af53541f23f6a950
Alhajideen <salifunurudeen834@gmail.com>
2022-09-06 14:06:15 -0600

    add an array for technologies in each of the objects in Projects array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,55 @@
 const projects = [
   {
     title: 'Project 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel eros interdum, malesuada erat quis, rhoncus nibh. Nam egestas sit amet sapien facilisis sollicitudin. Pellentesque rutrum felis nisl, a ultricies eros semper in. Aenean lacus elit, hendrerit nec erat nec, consequat vestibulum nibh.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel eros interdum, malesuada erat quis, rhoncus nibh. Nam egestas sit amet sapien facilisis sollicitudin. Pellentesque rutrum felis nisl, a ultricies eros semper in. Aenean lacus elit, hendrerit nec erat nec, consequat vestibulum nibh.',
     featured_image: './assets/img/projects-img1.png',
-    technologies: {
-      tec1: 'HTML/CSS',
-      tec2: 'Ruby on rails',
-      tec3: 'Javascript',
-    },
+    technologies: ['HTML/CSS', 'Ruby on rails', 'Javascript'],
     live_version: 'https://jfoyarzo.github.io/Project1',
     source: 'https://github.com/jfoyarzo/Project1',
   },
   {
     title: 'Project 2',
-    description: 'Quisque congue vitae leo vitae accumsan. Sed porttitor tellus ac orci sodales, sed eleifend magna commodo. Integer sed cursus sapien. Donec tristique neque a viverra rutrum. Curabitur eget aliquam leo. Nullam sed lectus imperdiet quam congue rutrum volutpat vitae lorem.',
+    description:
+      'Quisque congue vitae leo vitae accumsan. Sed porttitor tellus ac orci sodales, sed eleifend magna commodo. Integer sed cursus sapien. Donec tristique neque a viverra rutrum. Curabitur eget aliquam leo. Nullam sed lectus imperdiet quam congue rutrum volutpat vitae lorem.',
     featured_image: './assets/img/projects-img2.png',
-    technologies: {
-      tec1: 'HTML/CSS',
-      tec2: 'Ruby on rails',
-      tec3: 'Javascript',
-    },
+    technologies: ['HTML/CSS', 'Ruby on rails', 'Javascript'],
     live_version: 'https://jfoyarzo.github.io/Project2',
     source: 'https://github.com/jfoyarzo/Project2',
   },
   {
     title: 'Project 3',
-    description: 'Vestibulum efficitur lorem massa, eget viverra elit hendrerit sed. Donec nec imperdiet augue. Nullam non leo quis dui convallis vulputate id sit amet lectus. Ut gravida mattis diam vitae facilisis. Integer ut rutrum orci. Donec tempus at lectus ut eleifend.',
+    description:
+      'Vestibulum efficitur lorem massa, eget viverra elit hendrerit sed. Donec nec imperdiet augue. Nullam non leo quis dui convallis vulputate id sit amet lectus. Ut gravida mattis diam vitae facilisis. Integer ut rutrum orci. Donec tempus at lectus ut eleifend.',
     featured_image: './assets/img/projects-img3.png',
-    technologies: {
-      tec1: 'HTML/CSS',
-      tec2: 'Ruby on rails',
-      tec3: 'Javascript',
-    },
+    technologies: ['HTML/CSS', 'Ruby on rails', 'Javascript'],
     live_version: 'https://jfoyarzo.github.io/Project3',
     source: 'https://github.com/jfoyarzo/Project3',
   },
   {
     title: 'Project 4',
-    description: 'Vestibulum efficitur lorem massa, eget viverra elit hendrerit sed. Donec nec imperdiet augue. Nullam non leo quis dui convallis vulputate id sit amet lectus. Ut gravida mattis diam vitae facilisis. Integer ut rutrum orci. Donec tempus at lectus ut eleifend.',
+    description:
+      'Vestibulum efficitur lorem massa, eget viverra elit hendrerit sed. Donec nec imperdiet augue. Nullam non leo quis dui convallis vulputate id sit amet lectus. Ut gravida mattis diam vitae facilisis. Integer ut rutrum orci. Donec tempus at lectus ut eleifend.',
     featured_image: './assets/img/projects-img4.png',
-    technologies: {
-      tec1: 'HTML/CSS',
-      tec2: 'Ruby on rails',
-      tec3: 'Javascript',
-    },
+    technologies: ['HTML/CSS', 'Ruby on rails', 'Javascript'],
     live_version: 'https://jfoyarzo.github.io/Project4',
     source: 'https://github.com/jfoyarzo/Project4',
   },
   {
     title: 'Project 5',
-    description: 'Nunc feugiat lorem lorem, sit amet molestie ipsum porta eget. Sed bibendum diam cursus, scelerisque lacus lacinia, viverra mauris. Nunc at purus semper, ullamcorper leo sed, feugiat risus. Nulla facilisi. Curabitur venenatis lorem egestas, iaculis est ac, vulputate velit.',
+    description:
+      'Nunc feugiat lorem lorem, sit amet molestie ipsum porta eget. Sed bibendum diam cursus, scelerisque lacus lacinia, viverra mauris. Nunc at purus semper, ullamcorper leo sed, feugiat risus. Nulla facilisi. Curabitur venenatis lorem egestas, iaculis est ac, vulputate velit.',
     featured_image: './assets/img/projects-img5.png',
-    technologies: {
-      tec1: 'HTML/CSS',
-      tec2: 'Ruby on rails',
-      tec3: 'Javascript',
-    },
+    technologies: ['HTML/CSS', 'Ruby on rails', 'Javascript'],
     live_version: 'https://jfoyarzo.github.io/Project5',
     source: 'https://github.com/jfoyarzo/Project5',
   },
   {
     title: 'Project 6',
-    description: 'Quisque ante velit, tincidunt ac venenatis nec, rhoncus sed velit. Nam nulla metus, ultricies eu leo ac, auctor ultricies arcu. Suspendisse feugiat erat sit amet magna cursus tincidunt. Morbi scelerisque magna eget nunc egestas, sit amet porttitor mi commodo.',
+    description:
+      'Quisque ante velit, tincidunt ac venenatis nec, rhoncus sed velit. Nam nulla metus, ultricies eu leo ac, auctor ultricies arcu. Suspendisse feugiat erat sit amet magna cursus tincidunt. Morbi scelerisque magna eget nunc egestas, sit amet porttitor mi commodo.',
     featured_image: './assets/img/projects-img6.png',
-    technologies: {
-      tec1: 'HTML/CSS',
-      tec2: 'Ruby on rails',
-      tec3: 'Javascript',
-    },
+    technologies: ['HTML/CSS', 'Ruby on rails', 'Javascript'],
     live_version: 'https://jfoyarzo.github.io/Project6',
     source: 'https://github.com/jfoyarzo/Project6',
   },
@@ -89,3 +71,27 @@ closeX.addEventListener('click', toggleMenu);
 menuItems.forEach((e) => {
   e.addEventListener('click', toggleMenu);
 });
+
+let card='';
+for (let i = 0; i < projects.length; i++) {
+  let eachObj=projects[i];
+
+card += `
+ <div class="card card1">
+            <div class="card-image image1">
+              <img src="${eachObj.featured_image}" alt="">
+            </div>
+            <div class="card-body">
+              <h3 class="project-title">${eachObj.title}</h3>
+              <ul class="card-list">
+                <li class="card-list-item">HTML/CSS</li>
+                <li class="card-list-item">Ruby on Rails</li>
+                <li class="card-list-item">JavaScript</li>
+              </ul>
+              <button type="button" class="card-btn">
+                See this project &rarr;
+              </button>
+            </div>
+          </div>
+`;
+}
